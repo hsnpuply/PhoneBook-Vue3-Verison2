78f8b75ac71aecdb7fcbb358585509cfede09356
Azizi <azizi@csis.local>
2024-12-24 17:17:10 +0330

    code review

--- src/views/container/components/contactRecord.vue
+++ src/views/container/components/contactRecord.vue
@@ -1,40 +1,23 @@
 <script setup>
-import { reactive, ref, watch } from "vue";
+import { onMounted, reactive, ref, watch } from "vue";
 import { convertNumbersToPersian as PersianNumberConvertorX } from "@/utilities/functions";
 import moment from "moment-jalaali";
 import draggable from "vuedraggable";
 
 const props = defineProps({
-  LocalContacts: Array,
-  server_1_Contacts: Array,
-  DeleteLocalStorageContacts: Function,
+  data: Array,
+  DeleteContacts: Function,
   toggleEditForm: Function,
-});
-
-const state = reactive({
-  contacts: {
-    LocalContacts: props.LocalContacts,
-    server_1_Contacts: props.server_1_Contacts,
-  },
-});
+}); 
 </script>
 <template>
-  <draggable
-    v-model="state.contacts.LocalContacts"
-    tag="tbody"
-    class="text-xl bg-[#dddbdb] text-[#212222] overflow-hidden cursor-grab"
-  >
+  <draggable v-model="props.data" tag="tbody" class="text-xl bg-[#dddbdb] text-[#212222] overflow-hidden cursor-grab">
     <template #item="{ element, index }">
       <tr
-        class="text-right text-xl overflow-hidden even:bg-gray-200 bg-gray-400/50 cursor-pointer hover:bg-sky-900/60 hover:text-white duration-100 select-none"
-      >
+        class="text-right text-xl overflow-hidden even:bg-gray-200 bg-gray-400/50 cursor-pointer hover:bg-sky-900/60 hover:text-white duration-100 select-none">
         <td>{{ index + 1 }}</td>
         <td>
-          <v-avatar
-            variant="elevated"
-            class="!h-20 !w-20 my-2"
-            :image="element.avatar"
-          />
+          <v-avatar variant="elevated" class="!h-20 !w-20 my-2" :image="element.avatar" />
         </td>
         <td>{{ element.fullname }}</td>
         <td>{{ PersianNumberConvertorX(element.phoneNumber) }}</td>
@@ -49,30 +32,17 @@ const state = reactive({
         <td>{{ element.skills ? element.skills.join(" , ") : "" }}</td>
         <td>{{ element.favorites ? element.favorites.join(" , ") : "" }}</td>
         <td class="">
-          <div
-            class="actionButtonsContainer flex gap-2 items-center justify-center"
-          >
-            <v-btn
-              variant="elevated"
-              elevation="2"
-              prepend-icon="mdi-delete"
-              @click="
-                DeleteLocalStorageContacts(
-                  element.id,
-                  state.contacts.LocalContacts
-                )
-              "
-              class="bg-red-600/90 hover:bg-red-600/95"
-            >
+          <div class="actionButtonsContainer flex gap-2 items-center justify-center">
+            <v-btn variant="elevated" elevation="2" prepend-icon="mdi-delete" @click="
+              DeleteContacts(
+                element.id,
+                state.contacts.data
+              )
+              " class="bg-red-600/90 hover:bg-red-600/95">
               حذف
             </v-btn>
-            <v-btn
-              variant="elevated"
-              color="blue"
-              prepend-icon="mdi-account"
-              @click="toggleEditForm(element)"
-              class="bg-sky-600/90 hover:bg-sky-600/95"
-            >
+            <v-btn variant="elevated" color="blue" prepend-icon="mdi-account" @click="toggleEditForm(element)"
+              class="bg-sky-600/90 hover:bg-sky-600/95">
               ویرایش
             </v-btn>
           </div>
