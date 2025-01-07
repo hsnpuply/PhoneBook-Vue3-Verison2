e24b113991a01f6090e26663b20b73c71725a5f3
Hassan Barati <hsnpuply@gmail.com>
2025-01-07 16:32:48 +0330

    expanding

--- src/views/container/components/contactRecord myCopy.vue
+++ src/views/container/components/contactRecord myCopy.vue
@@ -50,22 +50,29 @@ const saveOrderLocal = ()=>{
     localStorage.setItem("contacts", JSON.stringify(localData.value));
     break;
     case 'Server':
-      console.log('lols');
+      // console.log('lols');
       
       break;
     
   }
 }
+
+const is_expanded =ref(false)
+
+// Reactive object to track expanded rows
+const expandedRows = reactive({});
+
+const toggle_expand = (id) => {
+  expandedRows[id] = !expandedRows[id]; // Toggle expansion for the specific row
+};
 </script>
 <template>
 
-  <draggable v-model="localData" group="allContacts" 
-  @end="saveOrderLocal"
-  tag="tbody"
-   class="select-none text-xl  bg-[#dddbdb] text-[#333333] cursor-grab">
+<draggable v-model="localData" group="allContacts" @end="saveOrderLocal" tag="tbody" class="select-none text-xl bg-[#dddbdb] text-[#333333] cursor-grab">
     <template #item="{ element, index }">
-      <tr class="h-2 hover:h-[10rem]  relative text-right hover:!bg-[#4c749f] hover:text-white duration-150  even:bg-gray-200 even:!bg-[#e0c083] bg-gray-400/50 !bg-[#f8f1e5] hover:bg-sky-900/60">
-        <td v-for="(col, i) in columnOrder" :key="i" class="">
+      <tr :class="expandedRows[element.id] ? '!h-[200px]' : ''"
+          class="relative overflow-hidden h-[20px] text-right hover:!bg-[#4c749f] hover:text-white duration-150 even:bg-gray-200 even:!bg-[#e0c083] bg-gray-400/50 !bg-[#f8f1e5] hover:bg-sky-900/60">
+        <td v-for="(col, i) in columnOrder" :key="i">
           <template v-if="fieldsMapping[col] === 'index'">
             {{ index + 1 }}
           </template>
@@ -73,40 +80,40 @@ const saveOrderLocal = ()=>{
             <v-avatar variant="elevated" class="!h-20 !w-20 my-2" :image="element.avatar" />
           </template>
           <template v-else-if="fieldsMapping[col] === 'selectedDate'">
-            {{
-              PersianNumberConvertorX(moment(element.selectedDate).format("jYYYY/jMM/jDD"))
-            }}
+            {{ PersianNumberConvertorX(moment(element.selectedDate).format("jYYYY/jMM/jDD")) }}
           </template>
           <template v-else-if="fieldsMapping[col] === 'isCoworker'">
             {{ element.isCoworker ? "بله" : "خیر" }}
           </template>
+          <template v-else-if="fieldsMapping[col] === 'skills'">
+            <p class="max-w-20 max-h-5 overflow-hidden " :class="expandedRows[element.id] ? 'max-w-44 max-h-full' : ''">
+              {{ element.skills.join(" , ") }}
+            </p>
+          </template>
           <template v-else-if="fieldsMapping[col] === 'actions'">
             <div class="actionButtonsContainer flex gap-2 items-center justify-center">
-            <v-btn variant="elevated" elevation="2" prepend-icon="mdi-delete" @click="
-              props.DeleteContacts(
-                element.id,
-                props.data
-              )
-              " class="bg-red-600/90 hover:bg-red-600/95">
-              حذف
-            </v-btn>
-            <v-btn variant="elevated" color="blue" prepend-icon="mdi-account" @click="props.toggleEditForm(element)"
-              class="bg-sky-600/90 hover:bg-sky-600/95">
-              ویرایش
-            </v-btn>
-          </div>
-
+              <v-btn variant="elevated" elevation="2" prepend-icon="mdi-delete" @click="props.DeleteContacts(element.id, props.data)"
+                class="bg-red-600/90 hover:bg-red-600/95">
+                حذف
+              </v-btn>
+              <v-btn variant="elevated" color="blue" prepend-icon="mdi-account" @click="props.toggleEditForm(element)"
+                class="bg-sky-600/90 hover:bg-sky-600/95">
+                ویرایش
+              </v-btn>
+            </div>
           </template>
           <template v-else>
             {{ Array.isArray(element[fieldsMapping[col]]) ? element[fieldsMapping[col]].join(", ") : element[fieldsMapping[col]] }}
           </template>
         </td>
-        <div class="group test absolute bottom-0 left-0 bg-black w-full text-center p-2">
-        <span class="">Lorem ipsum dolor sit amet.</span>
+
+        <!-- Display the expand toggle button only if the 'skills' array has more than 2 items -->
+        <div v-if="element.skills && element.skills.length > 2" class="expand_toggle py-2 absolute -bottom-6 right-[45%]" @click="toggle_expand(element.id)">
+          <v-btn icon="mdi-arrow-down" :class="expandedRows[element.id] ? 'rotate-180' : ''">
+          </v-btn>
         </div>
       </tr>
     </template>
-    
   </draggable>
 </template>
-<style></style>
+<style></style> 
